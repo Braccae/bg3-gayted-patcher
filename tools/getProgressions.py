43854910cea8ea8ec30d1299bdadc34b4b18f382
Braccae <andrew.scaccetti@outlook.com>
2023-10-23 14:16:26 -0400

    update getProgressions.py to be called as a class

--- tools/getProgressions.py
+++ tools/getProgressions.py
@@ -2,40 +2,43 @@ import os
 import shutil
 import argparse
 import re
+import sys
 
-# Define command line arguments
-parser = argparse.ArgumentParser(description='Search and copy Progressions.lsx files.')
-parser.add_argument('-i', '--input', help='Input directory (rawMods).')
-parser.add_argument('-o', '--output', help='Output directory (rawProgressions).')
-parser.add_argument('-v', '--verbose', action='store_true', help='Print each successful operation.')
+class ProgressionsExtractor:
+    def __init__(self, rawMods, rawProgressions):
+        self.rawMods = rawMods
+        self.rawProgressions = rawProgressions
+        # Define command line arguments
+        self.parser = argparse.ArgumentParser(description='Search and copy Progressions.lsx files.')
+        self.parser.add_argument('-v', '--verbose', action='store_true', help='Print each successful operation.')
 
-args = parser.parse_args()
+    def extract_progressions(self):
+        # Ensure output directory exists
+        os.makedirs(self.rawProgressions, exist_ok=True)
 
-# Get directories from environment variables if not provided in command line
-rawMods = args.input if args.input else os.getenv('rawMods')
-rawProgressions = args.output if args.output else os.getenv('rawProgressions')
+        # Walk through the directory
+        for root, dirs, files in os.walk(self.rawMods):
+            # Skip any directory named "Game"
+            dirs[:] = [d for d in dirs if d != "Game"]
+            
+            # Check if "Progressions.lsx" is in files
+            if "Progressions.lsx" in files:
+                # Get the folder name from the root directory
+                folder_name = os.path.basename(root)
+                
+                # Construct the destination file path
+                dest_file = os.path.join(self.rawProgressions, f"{folder_name}_Progressions.lsx")
+                
+                # Copy the file
+                shutil.copy2(os.path.join(root, "Progressions.lsx"), dest_file)
+                
+                # Print the operation if verbose flag is set
+                if args.verbose:
+                    print(f"Copied {root}/Progressions.lsx to {dest_file}", file=sys.stdout)
 
-# Ensure output directory exists
-os.makedirs(rawProgressions, exist_ok=True)
+# Example usage
+rawMods = 'rawmods'
+rawProgressions = 'rawprogressions'
 
-# Walk through the directory
-for root, dirs, files in os.walk(rawMods):
-    # Skip any directory named "Game"
-    dirs[:] = [d for d in dirs if d != "Game"]
-    
-    # Check if "Progressions.lsx" is in files
-    if "Progressions.lsx" in files:
-        # Construct the source file path
-        src_file = os.path.join(root, "Progressions.lsx")
-        
-        # Construct the destination file path
-        # Extract the folder name that is not "Game" using regex
-        folder_name = re.search(r'Public\\(.+?)\\Progressions', root).group(1)
-        dest_file = os.path.join(rawProgressions, f"{folder_name}_Progressions.lsx")
-        
-        # Copy the file
-        shutil.copy2(src_file, dest_file)
-        
-        # Print the operation if verbose flag is set
-        if args.verbose:
-            print(f"Copied {src_file} to {dest_file}")
+#extractor = ProgressionsExtractor(rawMods, rawProgressions)
+#extractor.extract_progressions()
